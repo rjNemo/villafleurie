61e070aa9adcd5f57b14769691845d3f31cf15fd
Ruidy Nemausat <nemausat@port-nemausat.local>
2019-12-03 17:47:31 +0100

    fix error pages urls

--- rental/views.py
+++ rental/views.py
@@ -168,8 +168,8 @@ class About(TemplateView):
 
 
 def handler404(request, exception):
-    return render(request, 'store/404.html', status=404)
+    return render(request, 'rental/404.html', status=404)
 
 
 def handler500(request):
-    return render(request, 'store/500.html', status=500)
+    return render(request, 'rental/500.html', status=500)
