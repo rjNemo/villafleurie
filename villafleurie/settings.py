9444d8354a7e0984cc5541c961304ed3acb858e0
Ruidy Nemausat <nemausat@port-nemausat.local>
2019-12-03 21:04:39 +0100

    staticfiles

--- villafleurie/settings.py
+++ villafleurie/settings.py
@@ -121,7 +121,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'staticfiles'),
+        os.path.join(PROJECT_ROOT, 'static'),
     )
 
     os.makedirs(STATIC_TMP, exist_ok=True)
