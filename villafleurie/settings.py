378bad800d5833696c6f77dc89a07e5b957c61ad
Ruidy Nemausat <nemausat@port-nemausat.local>
2019-12-03 17:21:10 +0100

    static folder settings

--- villafleurie/settings.py
+++ villafleurie/settings.py
@@ -113,8 +113,12 @@ if os.environ.get('ENV') == 'PRODUCTION':
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    STATIC_TMP = os.path.join(PROJECT_ROOT, 'static')
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
         os.path.join(PROJECT_ROOT, 'static'),
     )
+
+    os.makedirs(STATIC_TMP, exist_ok=True)
+    os.makedirs(STATIC_ROOT, exist_ok=True)
